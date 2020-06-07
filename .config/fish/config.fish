6c1b746f099437326ad53538700b085e2f05bcf9
saucoide <32314353+saucoide@users.noreply.github.com>
2020-06-07 12:11:04 +0200

    Initial commit
    ignore
    
    ignore pycache
    
    initial config
    
    new files
    
    + wallpapers
    
    random wallpapers, layout widget moved to the main bar, and shortcut changes
    
    udpated install script
    
    update qtile config
    
    fixed path
    
    autostart modified for arc+qtile+xfce pc
    
    changes for the laptop
    
    picom config from arcolinux
    
    some key binding changes, basic programs (file manager, text editor) on a constant on the top of the file
    
    removed ulauncher
    
    removing compton
    
    updated file manager, calendar and others
    
    +spotify
    
    changed to use kdewallet for storing secrets
    
    termite config added
    
    Adding rofi config, rofi theme, and key bindings to launch it
    
    adding termite config
    
    added -menu- icon to launch rofi
    
    changed group names
    
    group renaming + vifm
    
    adding bashrc
    
    a few laptop modifications
    
    battery and smaller fonts
    
    undoing the mess
    
    updated bashrc
    
    initial vifm config
    
    doom emacs config and other changes
    
    remapping capslock to control
    
    changed email client
    
    evolution instead of kmail
    
    +mu4e and other changes
    
    Switched all to a a literate config in org-mode
    
    The config lives now mostly in system.org, the generated files are also
    included in the repo
    Everything gets symlinked using GNUStow
    
    deleting old files
    
    changed Qtile colors & default programs
    
    changes to mu4e & smtp stuff
    
    changes to bashrc
    
    qtile config changes fixes after qtile 0.17
    
    more qtile config changes after 0.17 breakage
    
    removing doom's config
    
    qtile changes after the upgrade
    
    New emac's config
    
    renamed functions
    
    thinner logo
    
    added toggle functions from doom emac's
    
    +cider for clojure & some mu4e changes
    
    Added binding for flameshot
    
    minor changes
    
    disable modeline icons in windows
    
    fix org src block indendation & add clojure to languages
    
    moved auth sources into emacs init
    
    Agenda & Capture Templates
    
    fix polkit not properly starting
    
    fix openwith & mu4e interaction with attachments
    
    config changes
    
    fixed typo on numlock setxkbmap
    
    Initial org-roam setup
    
    emacs config update
    
    update for rofi 1.7.0
    
    qtile autostart changes
    
    + tangled autostart
    
    dont use tabs
    
    qtile autostart
    
    mu4e, lsp & elm config changes
    
    basic ranger config
    
    rofi config update & qtile polltext error handling
    
    Changing shell to fish
    
    fish config update
    
    +sudo
    
    make autostart.sh executable after tangle
    
    reorganized dotfiles
    
    fix paths
    
    small changes to aliases
    
    dired & ls changes
    
    bump
    
    delete stuff
    
    delete

---
+++ .config/fish/config.fish
@@ -0,0 +1,193 @@
+# PATH
+# ---------------------------------------------------------------------
+fish_add_path --prepend "~/.bin"
+fish_add_path --prepend "~/.local/bin"
+fish_add_path --prepend "~/.emacs.d/bin"
+fish_add_path --prepend "~/.poetry/bin"
+# ---------------------------------------------------------------------
+
+
+# Environment Variables
+# ---------------------------------------------------------------------
+set VISUAL "emacsclient -c -a ''"
+set EDITOR "emacsclient -t -a ''"
+set SSH_ENV "$HOME/.ssh/agent-environment"
+
+# fix for qtile & terminal apps resizing
+# (its fixed on master, should be able to remove it soon)
+set --erase COLUMNS
+set --erase LINES
+
+
+# Set vim as Manpager
+set --export MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+# ---------------------------------------------------------------------
+
+
+# Aliases
+# ---------------------------------------------------------------------
+## List - using exa as a replacement for ls 
+alias ls="lsd --long --color=always --group-dirs=first --date '+%Y-%m-%d %H:%M'"
+alias lsa="lsd --long --almost-all --group-dirs=first --date '+%Y-%m-%d %H:%M'"
+alias lst="lsd --long --tree --depth=2 --color=always --date '+%Y-%m-%d %H:%M'"
+## a better cat
+alias cat="bat"
+## I always miss the space
+alias cd..="cd .."
+## Colorize the grep command output 
+alias grep='grep --color=auto'
+## File system space info in readable format
+alias df='df --human-readable'
+## Memory info 
+alias free="free -mt"
+## Continue download
+alias wget="wget -c"
+## Userlist
+alias userlist="cut -d: -f1 /etc/passwd"
+## Aliases for software managment
+### Pacman
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu'
+### Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+### Yay as aur helper - updates everything
+alias yayupdate="yay -Syu"
+### Mirror updates
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+## Process info
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+## Arcolinux
+### copy/paste all content of /etc/skel over to home folder - backup of config created - beware
+alias skel='cp -Rf ~/.config ~/.config-backup-(date +%Y.%m.%d-%H.%M.%S) && cp -rf /etc/skel/* ~'
+## backup contents of /etc/skel to hidden backup folder in home/user
+alias bupskel='cp -Rf /etc/skel ~/.skel-backup-(date +%Y.%m.%d-%H.%M.%S)'
+## Hardware info --short
+alias hardware="hwinfo --short"
+## Calendar show full year
+alias cal="cal -y -m"
+## youtube-dl
+alias yta-best="youtube-dl --extract-audio --audio-format best "
+alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+alias yta-wav="youtube-dl --extract-audio --audio-format wav "
+alias ytv-best="youtube-dl -f bestvideo+bestaudio "
+# ---------------------------------------------------------------------
+
+
+# Functions
+# ---------------------------------------------------------------------
+
+# Startup greeter
+function fish_greeting
+    neofetch
+end
+
+# Use vim as default key bindings
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Function to extract a variety of archives
+# usage: extract <file>
+function extract
+  for arg in $argv
+    if test -f $arg
+      switch $arg
+        case "*tar.bz2" "*.tbz2"
+             tar xjf $arg
+        case "*tar.gz" "*.tgz"
+             tar xzf $arg
+        case "*.bz2"
+             bunzip2 $arg
+        case "*rar"
+             unrar x $arg
+        case "*.gz"
+             gunzip $arg
+        case "*.tar"
+             tar xf $arg
+        case "*.zip"
+             unzip $arg
+        case "*.Z"
+             uncompress $arg
+        case "*7z"
+             7z x $arg
+        case "*.deb"
+             ar x $arg
+        case "*tar.xz"
+             tar xz $arg
+        case "*tar.zst"
+             tar unzstd $arg
+        case "*"
+          set_color red
+          echo "I don't know how to extract this type of archive: `$arg`"
+          set_color normal
+      end
+    else
+        set_color red
+        echo "Not a valid file: `$arg`"
+        set_color normal
+    end
+  end
+end
+# ---------------------------------------------------------------------
+
+
+# Fish colors
+# ---------------------------------------------------------------------
+set fish_color_normal white
+set fish_color_command blue
+set fish_color_keyword yellow
+set fish_color_quote green
+set fish_color_error red
+set fish_color_param purple
+# fish_color_redirection
+# fish_color_end
+# fish_color_comment
+set fish_color_selection black
+# fish_color_operator
+# fish_color_escape
+set fish_color_autosuggestion "4c566a"
+# fish_color_cwd
+# fish_color_user
+# fish_color_host
+# fish_color_host_remote
+# fish_color_cancel
+# fish_color_search_match
+# ---------------------------------------------------------------------
+
+
+# PATH
+# TODO fix this
+# function start_agent {
+#     echo "Initialising new SSH agent..."
+#     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+#     echo succeeded
+#     chmod 600 "${SSH_ENV}"
+#     . "${SSH_ENV}" > /dev/null
+#     /usr/bin/ssh-add;
+# }
+
+# # Source SSH settings, if applicable
+
+# if [ -f "${SSH_ENV}" ]; then
+#     . "${SSH_ENV}" > /dev/null
+#     #ps ${SSH_AGENT_PID} doesn't work under cywgin
+#     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+#         start_agent;
+#     }
+# else
+#     start_agent;
+# fi
+
+# PROMPT (starship https://github.com/starship/starship)
+starship init fish | source
+
---
+++ .config/qtile/inoreader_rss_counter.py
@@ -0,0 +1,46 @@
+# Generated from ~/dotfiles/system.org
+
+import requests
+import keyring
+from pathlib import Path
+
+def main():
+
+    BASE_URL = "https://www.inoreader.com/reader/api/0"
+    LOGIN_URL = "https://www.inoreader.com/accounts/ClientLogin"
+
+    EMAIL = keyring.get_password("Passwords", 'ino_user')
+    PW = keyring.get_password("Passwords", 'ino_pw')
+    APP_ID = keyring.get_password("Passwords", 'ino_id')
+    APP_KEY = keyring.get_password("Passwords", 'ino_key')
+
+
+    resp = requests.post(LOGIN_URL, data={'Email':EMAIL,'Passwd':PW})
+
+    content = {}
+    for line in resp.text.split('\n'):
+        if line:
+            key, val = line.split("=")
+            content[key] = val
+    token = content['Auth']
+
+    headers = {'Authorization': 'GoogleLogin auth=' + token,
+            'Appid': APP_ID,
+            'AppKey': APP_KEY}
+
+    resp = requests.get(BASE_URL + "/unread-count", headers=headers)
+    unreadcounts = resp.json()
+    unread = unreadcounts['unreadcounts'][0]['count']
+
+    return str(unread)
+
+
+def main_wrapper():
+    try:
+        return main()
+    except:
+        return "Err"
+
+
+if __name__ == "__main__":
+    main_wrapper()
