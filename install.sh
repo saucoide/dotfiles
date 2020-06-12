3c8fe92b6e941fb854994cbecb5e148dc7d5c001
saucoide <saucoide@gmail.com>
2020-06-12 14:14:46 +0200

    udpated install script

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig config/qtile config/dunst config/compton.conf"    # list of files/folders to symlink in homedir
+dotfiles=~/dotfiles            # dotfiles directory
+backup=~/test_backup        # dotfiles backup directory
+files="gitconfig compton.conf config/qtile config/dunst config/ulauncher"           # list of files/folders to symlink in homedir
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+# create backup dir
+echo "Creating $backup for backup of any existing dotfiles in ~"
+mkdir -p $backup
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $dotfiles directory"
+cd $dotfiles
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
+    
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file $backup
+    echo "...done"
+    
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dotfiles/$file ~/.$file
+    echo "...done"
 done
