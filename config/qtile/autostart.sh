183431d309ddb0536261ee12e9312afcd6164f57
saucoide <saucoide@gmail.com>
2020-07-04 22:20:34 +0200

    +spotify

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -14,6 +14,7 @@ run nm-applet &
 blueberry-tray &
 run volumeicon &
 run xfce4-power-manager &
+run spotify
 
 picom --config $HOME/.config/qtile/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
