aa94ea79e8b08d4a0db47512457fd3fb7d5e08c7
saucoide <saucoide@gmail.com>
2021-09-06 00:57:57 +0200

    + tangled autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,13 +9,13 @@ function run {
   fi
 }
 
-dunst &
-numlockx on &
-run nm-applet &
-blueberry-tray &
-run volumeicon &
-run xfce4-power-manager &
+numlockx on
+setxkbmap -option "ctrl:nocaps"
+run dunst 
+run nm-applet
+run blueberry-tray
+run volumeicon
+run xfce4-power-manager
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run picom --config $HOME/.config/picom/picom.conf
 run spotify
-setxkbmap -option "ctrl:nocaps" &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom --config $HOME/.config/picom/picom.conf &
