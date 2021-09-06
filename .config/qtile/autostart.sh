5bfd2e6349590e19199134b09e8eeef57418e97d
saucoide <saucoide@gmail.com>
2021-09-07 01:21:41 +0200

    qtile autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,13 +9,13 @@ function run {
   fi
 }
 
-numlockx on
 setxkbmap -option "ctrl:nocaps"
-run dunst 
-run nm-applet
-run blueberry-tray
-run volumeicon
-run xfce4-power-manager
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-run picom --config $HOME/.config/picom/picom.conf
-run spotify
+run numlockx on
+run dunst &
+run nm-applet &
+run blueberry-tray &
+run volumeicon &
+run xfce4-power-manager &
+run picom --config $HOME/.config/picom/picom.conf &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run spotify &
