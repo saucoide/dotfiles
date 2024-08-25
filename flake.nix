e8121ab577fc1e965a8e2c01dccb55efd3a88b84
saucoide <saucoide@gmail.com>
2024-08-25 18:31:56 +0200

    checkpoint

--- flake.nix
+++ flake.nix
@@ -1,29 +1,29 @@
-
 {
-description = "Test Flake";
+  description = "System Flake";
 
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  home-manager = {
-	  url = "github:nix-community/home-manager";
-	  inputs.nixpkgs.follows = "nixpkgs";
-  };
-};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-outputs = { self, nixpkgs, home-manager, ...}: {
-  nixosConfigurations = {
-    zima = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./zima.nix
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.saucoide = import ./home.nix;
-	}
-      ];
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-};
+
+  outputs = { self, nixpkgs, home-manager, ...}: {
+    nixosConfigurations = {
+      zima = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./zima.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.saucoide = import ./home.nix;
+	  }
+	];
+      };
+    };
+  };
 }
--- zima.nix
+++ zima.nix
@@ -53,11 +53,30 @@
     # bluetooth.settings = {}
   };
 
-  # Keyboard settings
+  # X 
   services.xserver = {
+    enable = true;
+    videoDrivers = ["modesetting"];  # others: "intel" (old)  "amdgpu-pro" or "nvidia"
+
+    ## Window Manager / Qtile
+    windowManager.qtile = {
+      enable = true;
+      # configFile = "$HOME/dotfiles/.config/qtile/config.py";
+      extraPackages = python3Packages: with python3Packages; [
+          pulsectl-asyncio
+      ];
+    };
+
+    # Keyboard Settings
     layout = "us";
     xkbVariant = "";
   };
+
+  services.libinput = {
+    enable = true;
+    touchpad.tapping = true;
+  };
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.saucoide = {
@@ -89,38 +108,34 @@
   programs.fish.enable = true;
 
   # Services
+# 
+#   ## Display Manager / Login Screen
+#   services.greetd = {
+#     enable = true;
+#     settings = {
+#       default_session = {
+#         command = "${pkgs.greetd.tuigreet}/bin/tuigreet \\
+#                       --remember \\
+#                       --user-menu \\
+#                       --asterisks \\
+#                       --time \\
+#                       --time-format '%F %R' \\
+#                       --window-padding 0 \\
+#                       --container-padding 2 \\
+#                       --cmd Hyprland";
+# 	user = "greeter";
+#       };
+#     restart = true;
+#     };
+#   };
 
-  ## Display Manager / Login Screen
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet \\
-                      --remember \\
-                      --user-menu \\
-                      --asterisks \\
-                      --time \\
-                      --time-format '%F %R' \\
-                      --window-padding 0 \\
-                      --container-padding 2 \\
-                      --cmd Hyprland";
-	user = "greeter";
-      };
-    restart = true;
-    };
-  };
+    
 
-  ## Window Manager
-  programs.hyprland.enable = true;
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  };
-
-  ## Hint electron apps to use wayland
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
+#   programs.hyprland.enable = true;
+#   xdg.portal = {
+#     enable = true;
+#     extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+#   };
 
   ## Sound with pipewire
   security.rtkit.enable = true;
@@ -136,9 +151,9 @@
   # nixpkgs.config.pulseaudio = true;
   
   # Fonts
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = ["FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
-  ];
+  # fonts.packages = with pkgs; [
+  #   (nerdfonts.override { fonts = ["FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -165,6 +180,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";  # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
