a220548aa9298d114ea53b6f24a8211d393f3a71
sauco <sauco.navarro@team.wrike.com>
2026-01-15 11:29:34 +0100

    enable surround

--- modules/neovim/lua/plugins/nvim-surround.lua
+++ modules/neovim/lua/plugins/nvim-surround.lua
@@ -4,10 +4,10 @@ return {
 		enabled = true,
 		version = "^3.0.0",
 		event = "VeryLazy",
-		-- config = function()
-		-- 	require("nvim-surround").setup({
-		-- 		-- Configuration here, or leave empty to use defaults
-		-- 	})
-		-- end
-	}
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
