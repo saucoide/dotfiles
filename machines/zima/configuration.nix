628525e5d772a47160fa64548284e75570e6b146
saucoide <saucoide@gmail.com>
2026-01-20 01:50:06 +0100

    + nh

--- machines/zima/configuration.nix
+++ machines/zima/configuration.nix
@@ -29,6 +29,14 @@
     dates = "weekly";
     options = "--delete-older-than 15d";
   };
+
+  programs.nh = {
+    enable = true;
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 4d --keep 3";
+    # flake = "/home/user/my-nixos-config"; # sets NH_OS_FLAKE variable for you
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   # Bootloader.
