c52b64a843af59b95d28db0bb46b9bfa7d8391c3
saucoide <saucoide@gmail.com>
2026-01-22 14:28:02 +0100

    themes + zeal

--- modules/home/themes.nix
+++ modules/home/themes.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  # GTK programs
   gtk = {
     enable = true;
     theme = {
@@ -21,6 +22,24 @@
     };
   };
 
+  # Qt programs
+  qt = {
+    enable = true;
+    platformTheme.name = "kvantum";
+    style.name = "kvantum";
+  };
+
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=Qogir-dark
+  '';
+  home.packages = with pkgs; [
+    qogir-kde # The specific Qt/Kvantum assets for Qogir
+    libsForQt5.qtstyleplugin-kvantum # Qt5 engine
+    kdePackages.qtstyleplugin-kvantum # Qt6 engine (crucial for modern apps)
+  ];
+
+  # Cursor
   home.pointerCursor = {
     name = "phinger-cursors-light";
     package = pkgs.phinger-cursors;
